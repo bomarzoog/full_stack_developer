24686dedb653f07e1acc9647d1425dd7637971a9
Bomarzoog <bomarzoogo@gmail.com>
2021-12-27 16:12:07 -0600

    change

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,15 +1,31 @@
 from  flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Define flask app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///db2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'postgresql:///db2'
+
+
+#Map flask app to database
+db = SQLAlchemy(app)
+
+#Create db table 
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Columon(db.Integer,primary_key=True)
+    description = db.Columon(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}'
+
+db.create_all()
+    
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-        }, {
-            'description':'Todo 2'
-        }, {
-            'description': 'Todo 3'
-        }])
+    return render_template('index.html', data=Todo.query.all())
 
 
 if __name__=="__main__":
