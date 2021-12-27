924e415e238c57e7fde6136ecd871e88aaad83c9
Bomarzoog <bomarzoogo@gmail.com>
2021-12-27 16:32:14 -0600

    change

--- todoapp/app.py
+++ todoapp/app.py
@@ -17,20 +17,15 @@ class Todo(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     description = db.Column(db.String(), nullable=False)
 
-    #def __repr__(self):
-    #   return f'<Todo {self.id} {self.description}>'
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
     
-data1 = Todo.query.all()
-
-print (data1[1].description)
-
-
-#@app.route('/')
-#def index():
-#    return render_template('index.html', data=Todo.query.all())
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
 
 
-#if __name__=="__main__":
-#   app.run(host="0.0.0.0")
+if __name__=="__main__":
+   app.run(host="0.0.0.0")
