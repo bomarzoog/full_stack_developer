dd60a423d07aba7e675dea5ffd5ebb86584f2ede
Bomarzoog <bomarzoogo@gmail.com>
2021-12-27 19:49:54 -0600

    change

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,4 +1,4 @@
-from  flask import Flask, render_template
+from  flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -22,10 +22,19 @@ class Todo(db.Model):
 
 db.create_all()
     
+
+
+@app.route('/todo/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description','')
+    todo =Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
-
 
 if __name__=="__main__":
    app.run(host="0.0.0.0")
