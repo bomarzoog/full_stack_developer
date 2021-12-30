a0eb79fd099747e8fdb9f663187091390a3e6481
Bomarzoog <bomarzoogo@gmail.com>
2021-12-30 17:48:10 -0600

    add completed

--- todoapp/app.py
+++ todoapp/app.py
@@ -50,9 +50,23 @@ def create_todo():
     else:
         return jsonify(body)
 
+@app.route ('/todo/<todo_id>/check-completed', methods=['POST'])
+def check_completed(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo =Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:ß
+        db.session.close()
+    return redirect(url_for('index'))
+    
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 if __name__=="__main__":
    app.run(host="0.0.0.0")
