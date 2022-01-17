89afefb98d2c14dedfb157ceae22342c60eed558
Bomarzoog <bomarzoogo@gmail.com>
2022-01-17 16:58:20 -0600

    mo

--- cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/setup.sh
+++ cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/setup.sh
@@ -4,4 +4,4 @@ pip3 install flask --upgrade
 pip3 uninstall flask-socketio -y
 service postgresql start
 su - postgres bash -c "psql < /var/lib/postgresql/full_stack_developer/cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/setup.sql"
-su - postgres bash -c "psql bookshelf < /var/lib/postgresql/full_stack_developer/cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/books.psql"
+su - postgres bash -c "psql bookshelf < /var/lib/postgresql/full_stack_developer/cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/books.psql"
