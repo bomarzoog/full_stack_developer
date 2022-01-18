6b787b20963d4ac2e26d6198c50e7ff532f363e0
Bomarzoog <bomarzoogo@gmail.com>
2022-01-17 20:57:15 -0600

    mo

--- cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/flaskr/__init__.py
+++ cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/flaskr/__init__.py
@@ -63,7 +63,7 @@ def create_app(test_config=None):
         return result
     
 
-    @app.route('/books/<int:book_id>' methods=["DELETE"])
+    @app.route('/books/<int:book_id>', methods=["DELETE"])
     def deleteBook(book_id):
         try:
             book = Book.query.filte(Book.id==book_id).firs()
