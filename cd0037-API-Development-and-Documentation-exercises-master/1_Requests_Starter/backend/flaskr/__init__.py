ec698e2632f54572f92a8768d7110558e78cf834
Bomarzoog <bomarzoogo@gmail.com>
2022-01-17 20:29:24 -0600

    mo

--- cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/flaskr/__init__.py
+++ cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/flaskr/__init__.py
@@ -52,7 +52,7 @@ def create_app(test_config=None):
     @app.route('/books/<int:book_id>', methods=["PATCH"])
     def changeRating(book_id):
         book = Book.query.filter(Book.id==book_id).first()
-        book.rating = request.json.get("rating")
+        book.rating = request.get_json("rating")
         result = jsonify(
             {
                 "success": True,
