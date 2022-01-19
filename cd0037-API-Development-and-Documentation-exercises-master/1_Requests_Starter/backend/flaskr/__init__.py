42081a8486a2dddf08e835c6c82f92f1e5f85a81
Bomarzoog <bomarzoogo@gmail.com>
2022-01-18 18:42:34 -0600

    mo

--- cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/flaskr/__init__.py
+++ cd0037-API-Development-and-Documentation-exercises-master/1_Requests_Starter/backend/flaskr/__init__.py
@@ -128,36 +128,36 @@ def create_app(test_config=None):
     
     @app.errorhandler(404)
     def not_found(error):
-    return jsonify({
-        "success": False, 
-        "error": 404,
-        "message": "Not found"
-        }), 404
+        return jsonify({
+            "success": False, 
+            "error": 404,
+            "message": "Not found",
+            }), 404
     
     @app.errorhandler(422)
     def unprocessable(error):
-    return jsonify({
-        "success": False, 
-        "error": 422,
-        "message": "unprocessable"
-        }), 422
+        return jsonify({
+            "success": False, 
+            "error": 422,
+            "message": "unprocessable"
+            }), 422
 
     @app.errorhandler(400)
     def bad_request(error):
-    return jsonify({
-        "success": False, 
-        "error": 400,
-        "message": "bad request"
-        }), 400
+        return jsonify({
+            "success": False, 
+            "error": 400,
+            "message": "bad request"
+            }), 400
 
 
     @app.errorhandler(405)
     def method_not_allowed(error):
-    return jsonify({
-        "success": False, 
-        "error": 405,
-        "message": "method not allowed"
-        }), 405
+        return jsonify({
+            "success": False, 
+            "error": 405,
+            "message": "method not allowed"
+            }), 405
     
     
     
