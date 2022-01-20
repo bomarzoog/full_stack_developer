869dbb590f81c35d1c8854328381a2a3439deaaf
Bomarzoog <bomarzoogo@gmail.com>
2022-01-19 18:24:24 -0600

    test

--- cd0037-API-Development-and-Documentation-exercises-master/3_Testing_Starter/backend/test_flaskr.py
+++ cd0037-API-Development-and-Documentation-exercises-master/3_Testing_Starter/backend/test_flaskr.py
@@ -48,6 +48,63 @@ class BookTestCase(unittest.TestCase):
         self.assertEqual(data["success"], False)
         self.assertEqual(data["message"], 'resource not found')
 
+    def test_update_book_rating(self):
+        res = self.client().patch('/book/5', json={'rating': 3})
+        data = json.loads(res.data)
+        book = Book.query.filter(Book.id == 5).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(book.format()['rating'],1)
+
+    def test_400_for_faild_update(self):
+        res = self.client().patch('/book/5', json={'rating': 3})
+        data = json.loads(res.data)
+        book = Book.query.filter(Book.id == 5).one_or_none()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data['message'],'bad request')
+    
+    def test_create_new_book(self):
+        res = self.client().post("/books", json=self.new_book)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["created"])
+        self.assertTrue(len(data["books"]))
+
+    def test_405_if_book_creation_not_allowed(self):
+        res = self.client().post("/books/45", json=self.new_book)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "method not allowed")
+
+
+    def test_delete_book(self):
+        res = self.client().delete('/book/1')
+        data = json.loads(res.data)
+        book = Book.query.filter(Book.id == 1).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data['message'], 'bad request')
+        self.assertEqual(data['deleted'], 1)
+        self.assertTrue(data['total_books'])
+        self.assertTrue(data['books'])
+        self.assertEqual(book, None)
+
+    def test_422_if_book_does_not_exist(self):
+        res = self.client().delete("/books/1000")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")
+
 
 
 
